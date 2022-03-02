02b6d0dcc443bd8a87daf63c90a241ec339681e1
nutlope <he55@drexel.edu>
2022-03-01 18:34:45 -0800

    added v1 of version accounting for env variables

--- background.js
+++ background.js
@@ -4,7 +4,26 @@
 // * addListener - what we want to do with this event
 
 chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.update({
-    url: "https://vercel.com/new/git/external?repository-url=" + tab.url,
-  });
+  let vercelUrl =
+    "https://vercel.com/new/git/external?repository-url=" + tab.url;
+  let repoName = tab.url.substr(19);
+  let envFile = `https://raw.githubusercontent.com/${repoName}/main/.env.example`;
+
+  fetch(envFile)
+    .then((res) => res.text())
+    // if the .env file exists, open Vercel deploy with required env variables
+    .then((text) => {
+      let replaced = text.replace(/\s/g, "");
+      let newReplaced = replaced.replace(/=/g, ",");
+      vercelUrl += `&env=${newReplaced}`;
+      chrome.tabs.update({
+        url: vercelUrl,
+      });
+    })
+    // if the .example.env file doesn't exist, open Vercel deploy with no env variables
+    .catch((err) => {
+      chrome.tabs.update({
+        url: vercelUrl,
+      });
+    });
 });
